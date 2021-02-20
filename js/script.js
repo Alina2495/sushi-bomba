ec4c89a9355aa111a94cc0089c2659006f52d13f
alinanurislamova <alinanurislamova19@gmail.com>
2021-02-20 18:12:49 +0300

    20.02.21

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
-var swiper = new Swiper('.swiper-promotions', {
+var swiper = new Swiper('.swiper-banner', {
     slidesPerView: 1,
     speed: 400,
     spaceBetween: 30,
+    loop: true,
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    // },
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
@@ -14,15 +19,42 @@ var swiper = new Swiper('.swiper-promotions', {
         },
         // when window width is >= 480px
         480: {
-            slidesPerView: 2,
+            slidesPerView: 1,
             spaceBetween: 20
         },
         // when window width is >= 640px
         640: {
-            slidesPerView: 3,
+            slidesPerView: 1,
             spaceBetween: 30
         }
     },
+});
+
+var swiper = new Swiper('.promotions-swiper', {
+    slidesPerView: 1,
+    // speed: 400,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        576: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        }
+    }
 });
 
 var swiper = new Swiper('.swiper-comments', {
@@ -50,7 +82,7 @@ var swiper = new Swiper('.swiper-comments', {
         },
         // when window width is >= 640px
         640: {
-            slidesPerView: 3,
+            slidesPerView: 1,
             spaceBetween: 30
         }
     },
